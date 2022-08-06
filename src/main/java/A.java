e9e882dedf222499b15dff9ac247b068f3f0a69a
xiaoxuexue <xuexue@qq.com>
2022-08-06 11:12:02 +0800

    hot-fix second-commit2

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -5,5 +5,6 @@ public class A {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git hot-fix test");
     }
 }
