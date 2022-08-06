394fbe63ce50bf01e68cded834f8db9d16e8dbb8
xiaoxuexue <xuexue@qq.com>
2022-08-06 11:12:50 +0800

    master second-commit2

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -5,5 +5,6 @@ public class A {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git  master test");
     }
 }
