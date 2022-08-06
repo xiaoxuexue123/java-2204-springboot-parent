2e07a25d9339fbf283596b9a270accdec91848d3
xiaoxuexue <xuexue@qq.com>
2022-08-06 11:38:06 +0800

    push second-commit2

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -7,5 +7,6 @@ public class A {
         System.out.println("hello git");
         System.out.println("hello git hot-fix test");
         System.out.println("hello git  master test");
+        System.out.println("push  master test");
     }
 }
